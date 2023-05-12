9c68bc0617f0450390b55d6145e72aae0f02cc7b
Edward Kwartler <edwardkwartler@Edwards-MacBook-Air.local>
2023-05-12 12:26:28 -0400

    add and update some gpt scripts

---
+++ lessons/B_GPT_Robjects/scripts/D_gptCritique.R
@@ -0,0 +1,43 @@
+#' Author: Ted Kwartler
+#' Data: May 12, 2023
+#' Purpose: Let's use GPT to create a model and evaluate the respons as a class.
+
+
+# Prompt:
+# I have a dataframe in R called `df`.  It has the following columns with example data:
+# ```
+# df <- data.frame(category = c("a","a","b","a","c"),
+#                 input1   = c(1,2,3,1,1),
+#                 input2   = c(0,0,0,1,1),
+#                 input3   = c(F,F,T,T,F),
+#                 input4   = c(617.12,0,75.23,15.55,75.08),
+#                 y        = c(0,0,1,1,0))
+#```
+#
+#Can you please create a decision tree using this data?
+
+#GPT3.5 Response:
+#Sure!
+#To create a decision tree using the df dataframe, first, we'll need to install and load the rpart package.
+
+install.packages("rpart")
+library(rpart)
+
+#Next, we'll create the decision tree using the rpart() function. Since the dependent variable (y) is binary (0 or 1), we'll be using a classification tree.
+tree <- rpart(y ~ ., data = df, method = "class")
+
+#Here, we're telling R to build a decision tree that predicts y using all other columns in df.
+#Next, we'll visualize the decision tree using the rpart.plot package.
+
+install.packages("rpart.plot")
+library(rpart.plot)
+
+rpart.plot(tree)
+
+#This will give us a visual representation of the decision tree. The resulting plot will show the root node (the topmost node in the tree) and the various branches and leaf nodes that make up the tree. Each node in the tree represents a decision based on one of the predictor columns in df, and the branches represent the possible outcomes of that decision.
+
+# Here's the resulting decision tree:
+# Img not found
+
+#This decision tree shows us that the first decision point is based on category. If category is "b", then the tree predicts y = 1. If category is either "a" or "c", then the tree proceeds to the next decision point, based on the value of input3. If input3 is true, then the tree predicts y = 1. If input3 is false, then the tree predicts y = 0.
+
